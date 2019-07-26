e2569a65098b227ff5b4da9aa6ebedc946b8e9df
Kumar Shivendu <kshivendu@gmail.com>
2019-07-26 17:31:12 +0530

    build

---
+++ build/precache-manifest.0eea7f4ed23a8ad3256d673bb1aa8526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8a20d2531f5d08815d76a135b9694b",
+    "url": "/ReactContactManager/index.html"
+  },
+  {
+    "revision": "5da4ae9c550b68864df5",
+    "url": "/ReactContactManager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2779e8da4ecfe91e1a9d",
+    "url": "/ReactContactManager/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5da4ae9c550b68864df5",
+    "url": "/ReactContactManager/static/js/2.9e121fd8.chunk.js"
+  },
+  {
+    "revision": "2779e8da4ecfe91e1a9d",
+    "url": "/ReactContactManager/static/js/main.6142a0c1.chunk.js"
+  },
+  {
+    "revision": "78c9dbb3ce3622f9264f",
+    "url": "/ReactContactManager/static/js/runtime~main.9c43f537.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactContactManager/static/media/logo.5d5d9eef.svg"
+  }
+]);
